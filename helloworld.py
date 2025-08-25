cd3809ec5cc5e3d5ae88e28eb3ee40f44409aa98
Jing Liu <tommyliujing@gmail.com>
2025-08-25 15:56:57 +0200

    test numpy

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,5 @@
-#import numpy as np
+import numpy as np
 
-print(f"Hello world! You are in the main branch")
+a = np.array([1, 2, 3])
+print(a * 2)
+# print(f"Hello world! You are in the main branch")
